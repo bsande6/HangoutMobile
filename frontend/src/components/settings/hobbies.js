cce4acadfb65ecb7e48aac9855874e22b5ca460f
Kenley Shaw <ckshaw@g.clemson.edu>
2022-04-11 16:22:23 -0400

    formatting some things, expanding on hobbies page

--- frontend/src/components/settings/hobbies.js
+++ frontend/src/components/settings/hobbies.js
@@ -1,52 +1,67 @@
-import React, { useState, useEffect } from "react";
-import {
-  Box,
-  Stack,
-  Button,
-  Input,
-  InputLabel,
-  Typography,
-  Tab,
-  Tabs,
-  Select,
-  MenuItem,
-} from "@mui/material";
-import { useTheme } from "@mui/material";
-import axios from "axios";
-import PasswordChecklist from "react-password-checklist";
+import * as React from 'react';
+import { useTheme } from '@mui/material/styles';
+import OutlinedInput from '@mui/material/OutlinedInput';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
 import MainAppBar from '../homepage/mainmenu';
+import { Typography, Box, Button } from '@mui/material';
+import axios from "axios";
 
+const ITEM_HEIGHT = 48;
+const ITEM_PADDING_TOP = 8;
+const MenuProps = {
+  PaperProps: {
+    style: {
+      maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
+      width: 250,
+    },
+  },
+};
 
-const Hobbies = (props) => {
+const hobbies = [
+  'swimming',
+  'writing',
+  'drawing',
+  'acting',
+  'baking',
+  'reading',
+  'playing video games',
+  'running',
+  'walking',
+  'working out',
+  'biking',
+  'studying',
+  'cleaning',
+  'collecting',
+  'cooking',
+  'crafts',
+  'dancing',
+  'fashion',
+  'fishing',
+  'music',
+  'soccer',
+  'baseball',
+  'basketball',
+  'photography',
+  'singing',
+];
 
+export default function MultipleSelect() {
   const theme = useTheme()
-  const [user, setUser] = useState({
-    hobbie1: "",
-    hobbie2: "",
-    hobbie3: "",
-    hobbie4: "",
-    hobbie5: "",
-  });
-  const [hobbies, setHobbies] = useState([]);
+  const [user, setUserHobbies] = React.useState([]);
 
   const config = {
-    headers : {
-      "Authorization" : `Key key=123456789`,
-      "Content-Type" : "application/json",
-      "Cache-Control" : 'no-cache',
-      "Pragma" : 'no-cache',
-      "Expires" : '0'
+    headers: {
+      "Authorization": `Key key=123456789`,
+      "Content-Type": "application/json",
+      "Cache-Control": 'no-cache',
+      "Pragma": 'no-cache',
+      "Expires": '0'
     }
   }
-
-  const handleChange = (event) => {
-    const value = event.target.value;
-    setUser({
-      ...user,
-      [event.target.name]: value,
-    });
-  };
-
+  
   const handleSubmit = (event) => {
     console.log(JSON.stringify(user));
     axios
@@ -55,9 +70,18 @@ const Hobbies = (props) => {
       .catch((err) => console.log(err));
   };
 
+  const handleChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+    setUserHobbies(
+      typeof value === 'string' ? value.split(',') : value,
+    );
+  };
+
   return (
     <div>
-                <MainAppBar/>
+      <MainAppBar />
       <Typography align="center" variant="h3">
         Choose 5 Hobbies
       </Typography>
@@ -68,70 +92,31 @@ const Hobbies = (props) => {
           alignContent: "center",
         }}
       >
-        <Stack spacing={2} sx={{ width: "50vw" }}>
-            <Select
-              value={user.hobbies}
-              autoWidth
-              label="hobbies"
-              name="hobbies"
-              onChange={handleChange}
-            >
-              {hobbies.map((hobbies) => (
-                <MenuItem value={hobbies.id}>{hobbies.name}</MenuItem>
-              ))}
-            </Select>
-            <Select
-              value={user.hobbies}
-              autoWidth
-              label="hobbies"
-              name="hobbies"
-              onChange={handleChange}
-            >
-              {hobbies.map((hobbies) => (
-                <MenuItem value={hobbies.id}>{hobbies.name}</MenuItem>
-              ))}
-            </Select>
-            <Select
-              value={user.hobbies}
-              autoWidth
-              label="hobbies"
-              name="hobbies"
-              onChange={handleChange}
-            >
-              {hobbies.map((hobbies) => (
-                <MenuItem value={hobbies.id}>{hobbies.name}</MenuItem>
-              ))}
-            </Select>
-            <Select
-              value={user.hobbies}
-              autoWidth
-              label="hobbies"
-              name="hobbies"
-              onChange={handleChange}
-            >
-              {hobbies.map((hobbies) => (
-                <MenuItem value={hobbies.id}>{hobbies.name}</MenuItem>
-              ))}
-            </Select>
-            <Select
-              value={user.hobbies}
-              autoWidth
-              label="hobbies"
-              name="hobbies"
-              onChange={handleChange}
-            >
-              {hobbies.map((hobbies) => (
-                <MenuItem value={hobbies.id}>{hobbies.name}</MenuItem>
-              ))}
-            </Select>
-          <Button variant="contained" disableFocusRipple={true} sx={{color: theme.palette.text.primary }} onClick={handleSubmit}>
-            Done
-          </Button>
-        </Stack>
-
+        <FormControl sx={{ m: 1, width: 300 }}>
+          <InputLabel id="hobbies">Hobbies</InputLabel>
+          <Select
+            labelId="hobbies"
+            id="hobbies"
+            multiple
+            value={user}
+            onChange={handleChange}
+            input={<OutlinedInput label="Hobbie" />}
+            MenuProps={MenuProps}
+          >
+            {hobbies.map((hobbie) => (
+              <MenuItem
+                key={hobbie}
+                value={hobbie}
+              >
+                {hobbie}
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+        <Button variant="contained" disableFocusRipple={true} sx={{ color: theme.palette.text.primary }} onClick={handleSubmit}>
+          Done
+        </Button>
       </Box>
     </div>
   );
-};
-
-export default Hobbies;
+}
